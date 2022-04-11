c7ebf7487a6d43ed1ab2d8669c655719a1c43daa
Felipe Miotto <fmiotto5@ucs.br>
2022-04-11 17:26:24 -0300

    Adicionando e remocendo tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,45 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+	// Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+	
+	if (newTaskTitle.trim().length === 0) {
+		return;
+	}
+
+	const newTask: Task = {
+		id: Math.floor(Math.random() * 1000),
+		title: newTaskTitle,
+		isComplete: false
+	}
+
+	setTasks([...tasks, newTask]);
+	
+	setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+	  
+	const newTasks: Task[] = tasks.map(task => {
+		if (task.id === id) {
+			task.isComplete = !task.isComplete;
+		}
+
+		return task;
+	});
+
+	setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+	const newTasks: Task[] = tasks.filter(task => {
+		return task.id !== id;
+	});
+
+	setTasks(newTasks);
   }
 
   return (
